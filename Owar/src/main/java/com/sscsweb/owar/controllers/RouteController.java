b875ee53a1d74f764603c04c4d2268f0593bd5d9
matteo cristoforo <matteo.cristoforo@gmail.com>
2015-12-03 12:04:32 +0100

    Project

---
+++ Owar/src/main/java/com/sscsweb/owar/controllers/RouteController.java
@@ -0,0 +1,16 @@
+package com.sscsweb.owar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RouteController {
+
+	@RequestMapping(value = "/")
+	public ModelAndView getHomePage() {
+		ModelAndView mv = new ModelAndView("home");
+		return mv;
+	}
+	
+}
